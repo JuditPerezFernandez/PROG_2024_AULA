05c4e88f9421eb376ce1df1e6938c602619f2027
Admin <Admin@LAPTOP-O93FSHCL>
2024-01-17 12:57:02 +0100

    Creamos manualmente 5 alumnos

--- src/Aula/Start.java
+++ src/Aula/Start.java
@@ -4,12 +4,25 @@ public class Start {
 
 	public static void main(String[] args) {
 		
-		Aula aula= new Aula(5);
-		for(int numAlumno=0;numAlumno<5;numAlumno++) {
-			Alumno alumno=new Alumno("Alumno "+numAlumno,15);
-			aula.anadirAlummno(alumno);
-		}
-		System.out.println(aula.toString());
+		Alumno a1=new Alumno();
+		a1.setNombre("Juan");
+		a1.setEdad(15);
+		
+		Alumno a2=new Alumno();
+		a2.setNombre("Marta");
+		a2.setEdad(25);
+		
+		Alumno a3=new Alumno();
+		a3.setNombre("Sara");
+		a3.setEdad(5);
+		
+		Alumno a4=new Alumno();
+		a4.setNombre("Luis");
+		a4.setEdad(19);
+		
+		Alumno a5=new Alumno();
+		a5.setNombre("Ivan");
+		a5.setEdad(9);
 	}
 
 }
